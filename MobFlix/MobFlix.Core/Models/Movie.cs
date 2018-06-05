ef5f68bc4d4ab9c9675174bf89e6e0bf673f16c1
Bas de Cort <bas.decort@infosupport.com>
2018-06-05 21:38:30 +0200

    Add model classes

---
+++ MobFlix/MobFlix.Core/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MobFlix.Core.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public int VoteCount { get; set; }
+        public string PosterPath { get; set; }
+        public List<int> GenreIds { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ MobFlix/MobFlix.Core/Models/Responses/SearchResultResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MobFlix.Core.Models.Responses
+{
+    public class SearchResultResponse
+    {
+        public int Page { get; set; }
+        public int TotalResults { get; set; }
+        public int TotalPages { get; set; }
+        public List<Movie> Movies { get; set; }
+    }
+}
