aab9337aeebcbbb4d96baeae6d1eb665c26bf90d
Bas de Cort <bas.decort@infosupport.com>
2018-06-05 21:39:15 +0200

    Add ViewModel and BaseViewModel

---
+++ MobFlix/MobFlix.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MobFlix.Core.ViewModels
+{
+    public class BaseViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MobFlix/MobFlix.Core/ViewModels/NowInCinemaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using MobFlix.Core.Models;
+using System.Threading.Tasks;
+using Refit;
+using MobFlix.Core.Services;
+
+namespace MobFlix.Core.ViewModels
+{
+    public class NowInCinemaViewModel : BaseViewModel
+    {
+        private List<Movie> _movies = new List<Movie>();
+        public List<Movie> Movies 
+        { 
+            get{ return _movies; } 
+            set
+            {
+                _movies = value;
+                RaisePropertyChanged();
+            } 
+        } 
+       
+        public async Task LoadAsync()
+        {
+            // TODO: replace with baseUrl
+            var movieService = RestService.For<IMovieService>("");
+            Movies = await movieService.GetAllInCinemaAsync().ConfigureAwait(false);
+        }
+    }
+}
