5f308a6edaf65b73c2330826e6982d77cee0beab
Bas de Cort <bas.decort@infosupport.com>
2018-06-05 21:38:58 +0200

    Add service to retrieve movies

---
+++ MobFlix/MobFlix.Core/Services/IMovieService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Refit;
+using MobFlix.Core.Models.Responses;
+
+namespace MobFlix.Core.Services
+{
+    public interface IMovieService
+    {
+        [Get("discover/movie?primary_release_date.gte=2018-05-01&primary_release_date.lte=2018-05-30")]
+        Task<SearchResultResponse> GetAllInCinemaAsync();
+    }
+}
