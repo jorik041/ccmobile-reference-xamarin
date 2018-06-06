a0346ffa5401c4e04d17e2521cc7757bc2a6aef4
Bas de Cort <bas.decort@infosupport.com>
2018-06-06 08:15:06 +0200

    Added MovieService

--- MobFlix/MobFlix.Core/Services/IMovieService.cs
+++ MobFlix/MobFlix.Core/Services/IMovieService.cs
@@ -6,7 +6,7 @@ namespace MobFlix.Core.Services
 {
     public interface IMovieService
     {
-        [Get("discover/movie?primary_release_date.gte=2018-05-01&primary_release_date.lte=2018-05-30")]
+        [Get("discover/movie?primary_release_date.gte=2018-05-01&primary_release_date.lte=2018-05-30&api_key=61847937aad8c2a54376001ce143f6cb")]
         Task<SearchResultResponse> GetAllInCinemaAsync();
     }
 }
---
+++ MobFlix/MobFlix.Core/Services/MovieDbServiceResolver.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+
+namespace MobFlix.Core.Services
+{
+    public static class MovieDbServiceResolver
+    {
+        private const string _imageBaseUrl = "https://image.tmdb.org/t/p/w185/";
+        private const string _apiKey = "61847937aad8c2a54376001ce143f6cb";
+        private const string _baseUrl = "https://api.themoviedb.org/3/";
+
+        public static T Resolve<T>()
+        {
+            return RestService.For<T>(_baseUrl);
+        }
+    }
+}
